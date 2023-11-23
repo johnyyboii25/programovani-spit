2a016b331f463711b6b9d42bfdd1b2770db59206
johnyyboii25 <145652196+johnyyboii25@users.noreply.github.com>
2023-11-23 10:08:36 +0100

    Add files via upload

---
+++ prog2311/prog2311/Program.cs
@@ -0,0 +1,45 @@
+﻿//int[] array = new int[] {-12,-5,-8,-64,-250,-13 };
+
+//int FindMax(int[] array)
+//{
+//	int max = array[0];
+//	for (int i = 0; i < array.Length; i++)
+//	{
+//		if (array[i] > max)
+//		{
+//			max = array[i];
+//		}
+//	}
+//	return max;
+////}
+
+
+//int maximum = FindMax(array);
+//Console.WriteLine($"maximum is {maximum}");
+
+int[] array = new int[] { 12, -5, -8, 64, 25, -13 };
+
+foreach (int i in array)
+{
+	Console.WriteLine(i);
+}
+
+static int FindMax(int[] array)
+{
+	int max = array[0];
+	int index = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] > max)
+		{
+			max = array[i];
+			index = i;
+		}
+
+	}
+	return index;
+}
+
+
+int maximum = FindMax(array);
+Console.WriteLine(maximum);
