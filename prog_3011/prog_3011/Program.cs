bd3946c3a197da7c4f033c067644c6ee42cdb425
johnyyboii25 <145652196+johnyyboii25@users.noreply.github.com>
2023-12-03 18:15:38 +0100

    Add files via upload

--- prog_3011/prog_3011/Program.cs
+++ prog_3011/prog_3011/Program.cs
@@ -1,2 +1,139 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+while (true)
+{
+	double DoubleParse()
+	{
+		bool success;
+		do
+		{
+			string ting = Console.ReadLine();
+			success = double.TryParse(ting, out double result);
+			return result;
+		} while (success == false);
+	}
+	int IntParse()
+	{
+		bool success;
+		do
+		{
+			string ting = Console.ReadLine();
+			success = int.TryParse(ting, out int result);
+			return result;
+		} while (success == false);
+	}
+
+	void PrintSquare()
+	{
+		Console.WriteLine("You have chosen square");
+		Console.WriteLine("Enter length of a side: ");
+		double a = DoubleParse();
+		Console.WriteLine($"Circumference: {4 * a}");
+		Console.WriteLine($"Area: {Math.Pow(a,2)}");
+	}
+	void PrintTriangle()
+	{
+
+	}
+	void PrintRectangle()
+	{
+		Console.WriteLine("You have chosen rectangle");
+		Console.WriteLine("Enter length of the reactangle: ");
+		double a = DoubleParse();
+		Console.WriteLine("Enter width of the rectangle: ");
+		double b = DoubleParse();
+		Console.WriteLine($"Circumference: {2*(a+b)}");
+		Console.WriteLine($"Area: {a * b}");
+	}
+	void PrintCircle()
+	{
+		Console.WriteLine("You have chosen circle");
+		Console.WriteLine("Enter length of the radius: ");
+		double r = DoubleParse();
+		Console.WriteLine($"Circumference: {2 * Math.PI * r}");
+		Console.WriteLine($"Area: {Math.PI * Math.Pow(r,2)}");
+	}
+	void PrintCube()
+	{
+		Console.WriteLine("You have chosen cube");
+		Console.WriteLine("Enter length of a side: ");
+		double a = DoubleParse();
+		Console.WriteLine($"Surface area: {6 * Math.Pow(a,2)}");
+		Console.WriteLine($"Volume: {Math.Pow(a,3)}");
+	}
+	void PrintCuboid()
+	{
+
+	}
+	void PrintSphere()
+	{
+		Console.WriteLine("You have chosen sphere");
+		Console.WriteLine("Enter length of the radius: ");
+		double r = DoubleParse();
+		Console.WriteLine($"Surface area: {4 * Math.PI * Math.Pow(r, 2)}");
+		Console.WriteLine($"Volume: {(4/3) * Math.PI * Math.Pow(r, 3)}");
+	}
+	void PrintCone()
+	{
+		Console.WriteLine("You have chosen cone");
+		Console.WriteLine("Enter radius : ");
+		double r = DoubleParse();
+		Console.WriteLine("Enter height: ");
+		double h = DoubleParse();
+		Console.WriteLine($"Surface area: {Math.PI * r * (r + Math.Sqrt((Math.Pow(r, 2) + Math.Pow(h, 2))))}");
+		Console.WriteLine($"Volume: {(1/3) * Math.PI * h * Math.Pow(r, 2)}");
+	}
+
+	Console.WriteLine("Select shape with index:");
+	Console.WriteLine("1 for square");
+	Console.WriteLine("2 for rectangle");
+	Console.WriteLine("3 for triangle");
+	Console.WriteLine("4 for circle");
+	Console.WriteLine("5 for cube");
+	Console.WriteLine("6 for sphere");
+	Console.WriteLine("7 for cuboid");
+	Console.WriteLine("8 for cone");
+	int volba = IntParse();
+	switch (volba)
+
+	{
+
+		case 1:
+			PrintSquare();
+			break;
+		case 2:
+			PrintRectangle();
+			break;
+		case 3:
+			PrintTriangle();
+			break;
+		case 4:
+			PrintCircle();
+			break;
+		case 5:
+			PrintCube();
+			break;
+		case 6:
+			PrintSphere();
+			break;
+		case 7:
+			PrintCuboid();
+			break;
+		case 8:
+			PrintCone();
+			break;
+		case 9:
+			return;
+		default:
+			Console.WriteLine("Neplatná volba.");
+			break;
+
+	}
+
+	Console.WriteLine("\nStiskněte libovolnou klávesu pro pokračování.");
+
+	Console.ReadKey();
+
+	Console.Clear(); // Vymaže konzoli pro další volby
+
+}
