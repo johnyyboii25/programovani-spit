ee36e5800d754fb382070b5b0499e59a1861a58d
johnyyboii25 <145652196+johnyyboii25@users.noreply.github.com>
2024-01-04 09:44:11 +0100

    Add files via upload

---
+++ prog_2112/prog_2112/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] pokemons = File.ReadAllLines("pokedexnew.txt");
+string longestName = "";
+foreach (string pokemon in pokemons)
+{
+	string[] parts = pokemon.Split((char)44);
+	Console.WriteLine(parts[1]);
+	Console.WriteLine($"{parts[1]} Type: {parts[2]}");
+	Console.WriteLine($"{parts[1]} Health: {parts[5]}");
+	Console.WriteLine();
+
+	if (parts[1].Length > longestName.Length)
+	{
+		longestName = parts[1];
+	}
+}
+
+Console.WriteLine(longestName);
+//while (true)
+//{
+//	var rnd = new Random();
+//	int i = rnd.Next(720);
+//	Console.WriteLine(pokemons[i]);
+//	Console.ReadLine();	
+//}
