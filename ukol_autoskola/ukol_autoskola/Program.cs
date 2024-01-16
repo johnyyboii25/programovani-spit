55aa9d8f02287537cd8c9761a1370bf7760294d7
johnyyboii25 <145652196+johnyyboii25@users.noreply.github.com>
2024-01-16 23:30:08 +0100

    Add files via upload

---
+++ ukol_autoskola/ukol_autoskola/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Globalization;
+
+while (true)
+{
+	Console.WriteLine("Kvíz z autoškoly.");
+	Console.WriteLine("Pro skončení programu stiskněte q.");
+	Console.WriteLine();
+
+	LoadQuestion("questions.data");
+}
+
+
+string LoadQuestion(string path)
+{
+	string[] questions = File.ReadAllLines(path);
+
+	string[] parts = questions.Split((char)124);
+	Console.WriteLine(parts[0]);
+	for (int i = 3; i < parts.Length - 1; i++)
+	{
+		Console.WriteLine(parts[i]);
+	}
+	Console.WriteLine($"Spravna odpoved {parts[2]}");
+
+	return "0";
+}
