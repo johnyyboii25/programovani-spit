50180091ade3bb73d28c9443e7635edee0bf4c4d
johnyyboii25 <145652196+johnyyboii25@users.noreply.github.com>
2024-02-22 09:42:55 +0100

    Add files via upload

---
+++ prog_2202/prog_2202/Program.cs
@@ -0,0 +1,8 @@
+﻿Stack stack = new Stack(5);
+stack.Add(12);
+Console.WriteLine(stack.Get());
+stack.Add(1);
+stack.Add(5234);
+stack.Add(8);
+Console.WriteLine(stack.Get());
+Console.WriteLine(stack.Get());
---
+++ prog_2202/prog_2202/Stack.cs
@@ -0,0 +1,25 @@
+﻿class Stack
+{
+	int[] array;
+	int LastIndex;
+
+	public Stack(int capacity)
+	{
+		array = new int[capacity];
+	}
+
+	public void Add(int value)
+	{
+		array[LastIndex++] = value;
+		LastIndex++;
+		Console.WriteLine();
+	}
+
+	public int Get()
+	{
+		int value = array[LastIndex];
+		LastIndex--;
+		Console.WriteLine();
+		return value;
+	}
+}
